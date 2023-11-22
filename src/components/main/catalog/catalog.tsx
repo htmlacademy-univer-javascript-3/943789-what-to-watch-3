9964002f3cc4468baf0309cc6a93b7451603e43c
Khramov_Egor <khramovegor@gmail.com>
2023-11-22 23:50:01 +0500

    fixes from review

--- src/components/main/catalog/catalog.tsx
+++ src/components/main/catalog/catalog.tsx
@@ -1,32 +1,37 @@
 import FilmCardsList from '../../film-list/film-cards-list';
 import { useAppDispatch, useAppSelector } from '../../../hooks';
-import { GenresFilter } from '../../../data/films/genres-filter';
 import classNames from 'classnames';
-import { changeFilterGenreAction } from '../../../actions/action';
+import { changeFilterGenreAction, filterFilms } from '../../../actions/action';
 
-const allFilters = Object.values(GenresFilter) as Array<GenresFilter>;
+type Props = {
+  genres: Set<string>;
+}
 
-export function Catalog() {
+export function Catalog({ genres }: Props) {
   const dispatch = useAppDispatch();
 
-  const filterStore = useAppSelector((st) => st);
-
-  const films = useAppSelector((st) => st.films);
+  const selectedGenre = useAppSelector((store) => store.genre);
+  const films = useAppSelector((store) => store.films);
 
   return (
     <section className="catalog">
       <h2 className="catalog__title visually-hidden">Catalog</h2>
 
       <ul className="catalog__genres-list">
-        {allFilters.map((filter) => {
+        {[...genres].map((genre) => {
           const liClass = classNames({
             'catalog__genres-item': true,
-            'catalog__genres-item--active': filterStore.genreFilter === filter
+            'catalog__genres-item--active': selectedGenre === genre
           });
 
           return (
-            <li className={liClass} key={filter}>
-              <a className='catalog__genres-link' onClick={() => dispatch(changeFilterGenreAction(filter))}>{filter}</a>
+            <li className={liClass} key={genre}>
+              <a className='catalog__genres-link' onClick={() => {
+                dispatch(changeFilterGenreAction(genre));
+                dispatch(filterFilms());
+              }}
+              >{genre}
+              </a>
             </li>
           );
         })}
